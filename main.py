5d7c958e4e91be38584334a8e6cd2402ed19a8ef
agwaBom <khyunjin1993@gmail.com>
2023-04-06 16:36:41 +0900

    path_to_file_list implemented

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from typing import List
 
 def path_to_file_list(path: str) -> List[str]:
     """Reads a file and returns a list of lines in the file"""
-    # Will this be working?
+
+def path_to_file_list(path: str) -> List[str]:
+    """Reads a file and returns a list of lines in the file"""
     lines = open(path, 'r').read().split('\n')
     return lines
 

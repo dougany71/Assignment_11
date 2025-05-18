d81e2c875ea3c0100db8dbf5912f1b6ac71e7597
dougany71 <zobuzobuxomu@gmail.com>
2025-05-18 19:43:25 +0900

    Save current changes to main.py

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ from typing import List
 
 # Worked in collaboration with khyunjin1993:
 
-def path_to_file_list(path: str) -> List[str]:
-    """Reads a file and returns a list of lines in the file"""
+def process_file(file: str) -> str:
+    if '\\' in file:
+        file = file.replace('\\', '\\\\')
+    if '/' or '"' in file:
+        file = file.replace('/', '\\/')
+        file = file.replace('"', '\\"')
+    return file
 
 def path_to_file_list(path: str) -> List[str]:
     """Reads a file and returns a list of lines in the file"""
@@ -12,21 +17,10 @@ def path_to_file_list(path: str) -> List[str]:
 
 def train_file_list_to_json(english_file_list: List[str], german_file_list: List[str]) -> List[str]:
     """Converts two lists of file paths into a list of json strings"""
-    # Preprocess unwanted characters
-    def process_file(file):
-        if '\\' in file:
-            file = file.replace('\\', '\\\\')
-        if '/' or '"' in file:
-            file = file.replace('/', '\\/')
-            file = file.replace('"', '\\"')
-        return file
-
-    # Template for json file
     template_start = '{\"English\":\"'
     template_mid = '\",\"German\":\"'
     template_end = '\"}'
-
-def train_file_list_to_json(english_file_list: List[str], german_file_list: List[str]) -> List[str]:
+    
     processed_file_list = []
     for english_file, german_file in zip(english_file_list, german_file_list):
         english_file = process_file(english_file)
@@ -40,7 +34,7 @@ def write_file_list(file_list: List[str], path: str) -> None:
     with open(path, 'w') as f:
         for file in file_list:
             f.write(file + '\n')
-            
+
 if __name__ == "__main__":
     path = './'
     german_path = './german.txt'
@@ -51,4 +45,4 @@ if __name__ == "__main__":
 
     processed_file_list = train_file_list_to_json(english_file_list, german_file_list)
 
-    write_file_list(processed_file_list, path+'concated.json')
+    write_file_list(processed_file_list, path + 'concated.json')
